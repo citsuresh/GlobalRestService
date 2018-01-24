deacbf8fb1fc2c1d926bfa9a9b2dc3b161dfa9cd
Suresh Kumar Veluswamy <citsuresh@rediffmail.com>
2018-01-24 18:55:05 +0530

    Added clients api controller assets by client api controller

---
+++ GlobalRestService/Controllers/AssetsByClientController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GlobalRestService.Models;
+
+namespace GlobalRestService.Controllers
+{
+    public class AssetsByClientController : ApiController
+    {
+        private GlobalAssetRestServiceModel Db = new GlobalAssetRestServiceModel();
+
+        // GET api/<controller>
+        public IEnumerable<GlobalAsset> Get()
+        {
+            var clientIdKeyValuePair = Request.GetQueryNameValuePairs().Where(pair => String.Equals(pair.Key, "clientid", StringComparison.OrdinalIgnoreCase));
+
+            if (clientIdKeyValuePair.Any())
+            {
+                var clientId = clientIdKeyValuePair.First().Value;
+                return Db.GlobalAssets.Where(asset => asset.ClientID == clientId);
+            }
+
+            return Db.GlobalAssets.ToList();
+        }
+
+        // GET api/<controller>/clientId
+        public IEnumerable<GlobalAsset> Get(string clientid)
+        {
+            return Db.GlobalAssets.Where(asset => asset.ClientID == clientid);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GlobalRestService/Controllers/ClientsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GlobalRestService.Models;
+
+namespace AssetDBWebApi.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private GlobalAssetRestServiceModel Db = new GlobalAssetRestServiceModel();
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            var cientIds = Db.GlobalAssets.Select(asset => asset.ClientID).Distinct();
+
+            return cientIds;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
